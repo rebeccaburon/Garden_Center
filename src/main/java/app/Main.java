d721271650cb093fa3d599607df74886da8477fb
rebeccaburon <rebecca.mary.buron@hotmail.com>
2024-12-08 17:30:39 +0100

    Fixing workflow and main

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -8,6 +8,6 @@ public class Main {
     public static void main(String[] args) {
         EntityManagerFactory emf = HibernateConfig.getEntityManagerFactory("plants");
         AppConfig.startServer(emf);
-        System.out.println("YES");
+
     }
 }
